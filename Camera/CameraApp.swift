06f01f1485c090f5dbec90aa660ec1ba4794a540
Jason Rich Darmawan <63768126+jasonrichdarmawan@users.noreply.github.com>
2023-06-30 11:36:28 +0700

    Initial Commit

---
+++ Camera/CameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraApp.swift
+//  Camera
+//
+//  Created by Jason Rich Darmawan Onggo Putra on 30/06/23.
+//
+
+import SwiftUI
+
+@main
+struct CameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
